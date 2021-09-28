d52c455cf76c400b0173dcca2b2cf4374d3dc1ac
genaro21 <gallardo2147@gmail.com>
2021-09-28 19:55:54 +0200

    front

---
+++ pages/chat/_id.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="chat">
+        Hola chat
+    </div>
+</template>
+
+<script>
+export default {
+    async beforeMount() {
+        const token = window.localStorage.getItem('token')
+        if (!token) {
+            this.$router.push('./login')
+        }
+        await this.getMessages()       
+    },
+    methods: {
+        async getMessages() {
+            try{
+                const userOneId = window.localStorage.getItem('userId')
+                const userTwoId = this.$route.params.id
+
+                console.log({ userOneId, useTwoId})
+            //     const body = JSON.stringify({
+            //         userOne,
+            //         userTwo,
+            //     })
+            //    const res = await fetch('http://localhost:4500/message/chat', {
+            //        method: 'post',
+            //        body
+            //    }) 
+            //    const data = await res.json()
+            //    console.log({ data })
+            } catch (err) {
+                alert(err)
+            }
+        }
+    }
+    
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>Hola nuxt!</div>
+</template>
+
+
